5ffaccb608f5e23e22f674ceff4da2b0af45fe14
Yuri Gomes <yuri5_gomes@hotmail.com>
2022-09-06 20:29:59 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import NotificationButton from "./components/notificationButton"
-import SalesCard from "./components/SalesCard"
+import NotificationButton from "./components/notificationButton";
+import SalesCard from "./components/SalesCard";
 
 import Header from "./components/Header"
 
@@ -18,4 +18,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
