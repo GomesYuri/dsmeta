e959891a91a630bd3a81c86d0bf0a914ef09d200
Yuri Gomes <yuri5_gomes@hotmail.com>
2022-09-06 17:18:30 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/notificationButton"
+
 function App() {
-  return(
-    <h1>Ola mundo</h1>
+  return (
+    <>
+      <h1>Ola mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
