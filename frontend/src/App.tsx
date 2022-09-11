3ffb1755292ce59169f1f56485ef82cef2b95f25
Yuri Gomes <yuri5_gomes@hotmail.com>
2022-09-11 18:43:46 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import NotificationButton from "./components/notificationButton";
 import SalesCard from "./components/SalesCard";
 
@@ -6,6 +9,8 @@ import Header from "./components/Header"
 function App() {
   return (
     <>
+
+    <ToastContainer  />
       <Header />
       <main>
       <section id="sales">
